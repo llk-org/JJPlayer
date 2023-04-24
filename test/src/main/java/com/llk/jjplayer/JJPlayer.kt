b246708d4432ea66f0a5952215df2aef6c808f00
liangliangkai <liangkaileong@foxmail.com>
2023-04-24 22:14:30 +0800

    fix test 无法播放问题。

--- test/src/main/java/com/llk/jjplayer/JJPlayer.kt
+++ test/src/main/java/com/llk/jjplayer/JJPlayer.kt
@@ -4,17 +4,27 @@ import android.content.Context
 import android.opengl.GLSurfaceView
 import android.util.AttributeSet
 import android.view.SurfaceHolder
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
 /**
  * author: llk
  * date  : 2023/4/9
  * detail:
  */
-class JJPlayer: GLSurfaceView{
+class JJPlayer: GLSurfaceView, GLSurfaceView.Renderer {
 
-    constructor(context: Context) : super(context, null){ }
+    constructor(context: Context) : super(context, null){
+        init()
+    }
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){ }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+        init()
+    }
+
+    private fun init(){
+        setRenderer(this)
+    }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
         NativeUtils.surfaceCreated(holder.surface)
@@ -26,5 +36,14 @@ class JJPlayer: GLSurfaceView{
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
         NativeUtils.surfaceDestroyed(holder.surface)
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
     }
 }
